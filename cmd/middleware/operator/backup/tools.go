d85512a801d0c6d94f537236946475f0ebccc1b3
hys <hysyeah@gmail.com>
2024-11-08 14:41:44 +0800

    fix: change root path to olares

--- cmd/middleware/operator/backup/tools.go
+++ cmd/middleware/operator/backup/tools.go
@@ -1,7 +1,7 @@
 package backup
 
 func (w *Watcher) getMiddlewareBackupPath(_ string) (string, error) {
-	backupPath := "/terminus/rootfs/"
+	backupPath := "/olares/rootfs/"
 	backupPath += middleware_backup_path
 
 	return backupPath, nil
